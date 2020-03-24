114f2081287f788252cd2f1bc3cd6511d6c8605c
Travis Roberts <traviskroberts@gmail.com>
2020-03-23 23:31:40 -0500

    Add migrations to build script

--- build.sh
+++ build.sh
@@ -13,3 +13,6 @@ mix phx.digest
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
+
+# Run migrations
+_build/prod/rel/message_saver/bin/message_saver eval "MessageSaver.Release.migrate"
